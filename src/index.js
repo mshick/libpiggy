01bd91f4cc38799f86fad180ec9a40c5f89e1a71
Michael Shick <m@shick.us>
2017-04-17 12:02:22 -0700

    expose createTable

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ export {default as del} from './del';
 export {default as createConnection} from './create-connection';
 export {default as createNotifyTrigger} from './create-notify-trigger';
 export {default as createStore} from './create-store';
+export {default as createTable} from './create-table';
 export {default as createWatchedTable} from './create-watched-table';
 export {default as tableExists} from './table-exists';
 export {default as watchTable} from './watch-table';
