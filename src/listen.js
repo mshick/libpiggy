a23cae8e86d49deb3fe991f358e6a1c34f79e4d9
Michael Shick <m@shick.us>
2017-04-29 14:37:46 -0700

    try catch and return for watch table and listen

--- src/listen.js
+++ src/listen.js
@@ -1,5 +1,13 @@
-const listen = function ({client}) {
-  client.query(`LISTEN watchers`);
+const listen = async function ({client}) {
+  try {
+    await client.query(`LISTEN watchers`);
+    return {client};
+  } catch (error) {
+    return {
+      client,
+      error
+    };
+  }
 };
 
 export default listen;
--- src/watch-table.js
+++ src/watch-table.js
@@ -1,13 +1,20 @@
 import qs from 'qs';
 
 const watchTable = function ({client, table, watcher}) {
-  client.on('notification', ({channel, payload}) => {
-    const parsed = qs.parse(payload);
-    if (parsed.table === table) {
-      watcher({client, channel, payload, parsed});
-    }
-  });
-  client.query(`LISTEN watchers`);
+  try {
+    client.on('notification', ({channel, payload}) => {
+      const parsed = qs.parse(payload);
+      if (parsed.table === table) {
+        watcher({client, channel, payload, parsed});
+      }
+    });
+    return {client};
+  } catch (error) {
+    return {
+      client,
+      error
+    };
+  }
 };
 
 export default watchTable;
