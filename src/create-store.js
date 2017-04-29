a42a33bca111e177159999781b7d0e78620993af
Michael Shick <m@shick.us>
2017-04-29 12:53:08 -0700

    make exists check logic optional

--- src/create-store.js
+++ src/create-store.js
@@ -5,6 +5,7 @@ import createStoreIndexes from './create-store-indexes';
 import {applyToDefaults} from 'hoek';
 
 const defaults = {
+  checkExists: false,
   watch: false,
   ginIndex: {
     operator: 'jsonb_path_ops'
@@ -13,23 +14,28 @@ const defaults = {
 };
 
 const createStore = async function ({client, table, index, options}) {
-  const {watch, ginIndex, btreeIndex} = applyToDefaults(defaults, options || {});
+  const {watch, checkExists, ginIndex, btreeIndex} = applyToDefaults(defaults, options || {});
 
   try {
-    const results = await tableExists({client, table});
-    const {exists} = results;
+    let exists = false;
+
+    if (checkExists) {
+      const results = await tableExists({client, table});
+      exists = results;
+    }
 
     if (!exists) {
       const columns = 'key text primary key, val jsonb, created_at timestamp with time zone, updated_at timestamp with time zone';
+
       if (watch) {
         await createWatchedTable({client, table, columns, key: 'key'});
       } else {
         await createTable({client, table, columns});
       }
-    }
 
-    if (index) {
-      await createStoreIndexes({client, table, ginIndex, btreeIndex});
+      if (index) {
+        await createStoreIndexes({client, table, ginIndex, btreeIndex});
+      }
     }
 
     return {client};
--- src/create-watched-table.js
+++ src/create-watched-table.js
@@ -1,11 +1,13 @@
+import createTable from './create-table';
 import createNotifyTrigger from './create-notify-trigger';
 
 const createWatchedTable = async function ({client, table, columns, key}) {
   try {
     await createNotifyTrigger({client, key});
 
+    await createTable({client, table, columns});
+
     const text = `
-      CREATE TABLE IF NOT EXISTS ${table} (${columns});
       CREATE TRIGGER watched_table_trigger__${table}
       AFTER INSERT OR UPDATE ON ${table}
       FOR EACH ROW EXECUTE PROCEDURE notify_trigger();
